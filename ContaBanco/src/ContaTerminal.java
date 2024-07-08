68357b38a2ae43f96b87cbebd17065f45ea9f48b
wellington rafaelito rocha filho <81142734+wellingtonrochafilho@users.noreply.github.com>
2024-07-08 16:59:51 -0300

    finalizado

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -1,3 +1,26 @@
+import java.util.Scanner;
+
 public class ContaTerminal {
-    
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Qual o Numero da sua conta?");
+        int Numero = scanner.nextInt();
+
+        scanner.nextLine();
+
+        System.out.println("Qual é a Agencia da sua conta?");
+        String Agencia = scanner.nextLine();
+
+        System.out.println("Qual seu nome?");
+        String Cliente = scanner.nextLine();
+
+        System.out.println("Seu saldo é ");
+        double Saldo = scanner.nextDouble();
+
+        System.out.println("Olá" + Cliente + " , obrigado por criar uma conta em nosso banco, sua agência é " + Agencia
+                + " , conta" + Numero + " e seu saldo " + Saldo + " já está disponível para saque");
+
+        scanner.close();
+    }
 }
